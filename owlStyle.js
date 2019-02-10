e0037dbf5e3e94e2f0f71a8fb5353f03663f23cd
AlaaMostapha <alaamoustapha21@gmail.com>
2019-02-10 08:48:19 +0200

    Add files via upload

---
+++ owlStyle.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+     autoplay:false,
+      itemsCustom : [
+        [0, 2],
+        [450, 4],
+        [600, 7],
+        [700, 9],
+        [1000, 10],
+        [1200, 12],
+        [1400, 13],
+        [1600, 15]
+      ],
+      navigation : true
+  });
+});
